681746fe00716031373c16f4067f13b10c6faa3a
Kush-munot <munot.kgm@gmail.com>
2022-10-23 17:51:25 +0530

    UI Changes Done !! Start Skill section

--- my-portfolio/content/meta.js
+++ my-portfolio/content/meta.js
@@ -2,9 +2,9 @@ export default {
     home: {
       title: "",
       description:
-        "Hey, I am Jatin Sharma. A Front-end Developer/React Developer from India who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities. At the moment, I am pursuing my Bachelor's degree in Computer Science.",
-      image: "https://imgur.com/KeJgIVl.png",
-      keywords: "portfolio jatin, portfolio j471n, jatin blogs",
+        "Hey, I am Kush Munot. A Front-end Developer/React Developer from India who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities. At the moment, I am pursuing my Bachelor's degree in Computer Science.",
+      image: "https://i.imgur.com/aGW47qg.png",
+      keywords: "portfolio kush, portfolio Kush-munot, kush blogs,kush munot, kush munot linkedin",
     },
   
     stats: {
