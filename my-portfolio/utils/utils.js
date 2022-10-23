b16cb34bc6c3d05935df16431a17b96fa892bbf4
Kush-munot <munot.kgm@gmail.com>
2022-10-23 17:25:43 +0530

    Home Section Done !!

--- my-portfolio/utils/utils.js
+++ my-portfolio/utils/utils.js
@@ -10,4 +10,4 @@ export const navigationRoutes = [
     "rss",
 ];
 
-export const homeProfileImage = "https://user-images.githubusercontent.com/65546898/197387996-bc024982-553d-4e7e-8bc4-85b55e8da62d.png";
+export const homeProfileImage = "https://i.imgur.com/aGW47qg.png";
