c2ffe9886119c605c1d966b6ebd3552dc8f122d9
Kush-munot <munot.kgm@gmail.com>
2022-10-31 20:52:12 +0530

    Config files Updated

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,36 +1,3 @@
-/* const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-});
-
-const withPWA = require("next-pwa");
- */
-/* module.exports = withBundleAnalyzer(
-    withPWA({
-        webpack: true,
-        webpack: (config) => {
-            // Fixes npm packages that depend on `fs` module
-            config.resolve.fallback = { fs: false };
-            return config;
-        },
-        reactStrictMode: true,
-        images: {
-            domains: [
-                "i.imgur.com"
-            ],
-        },
-
-        // Pwa Setting
-        pwa: {
-            dest: "public",
-            register: true,
-            skipWaiting: true,
-            disable: process.env.NODE_ENV === "development",
-            publicExcludes: ["!resume.pdf"], // don't cache pdf which I'll add later
-        },
-    })
-); */
-
-
 module.exports ={
     reactStrictMode: true,
     images: {
